0ab18275afc89af3f972e4bfcc204465d7cea4e3
hernancasillas <hernancasillas@gmail.com>
2017-05-25 17:27:35 -0500

    Change Folder

--- Codigo Fuente/Pizza.java
+++ Codigo Fuente/Pizza.java
@@ -7,7 +7,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version 1.0
  */
 public class Pizza extends Food
-{
+{ 
     /**
      * Act - do whatever the Pizza wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
